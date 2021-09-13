50ef8f62fc39428bb21f6590f562b4bd5213d1df
Joao Batista <joaonetominato@gmail.com>
2021-09-13 04:54:12 -0300

    Aula 5 finalizada

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -37,7 +37,7 @@ function handleClick(event, check = true){
     modalDescription.innerHTML= `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
     modalButton.innerHTML= `Sim, ${text.toLowerCase()}`
     check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
-
+    
     //abrir modal
     modal.open()
 }
